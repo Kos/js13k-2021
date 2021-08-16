f0a2a8a1efca91ac6f1fb70dd0383c55806462b2
Tomasz Wesołowski <kosashi@gmail.com>
2021-08-17 00:16:15 +0200

    Project scaffolding with REGL and parcel

--- index.ts
+++ index.ts
@@ -1,1 +1,28 @@
-console.log("hello");
+import initRegl from "regl";
+const regl = initRegl();
+// @ts-ignore
+import vert from "./vert.glsl";
+// @ts-ignore
+import frag from "./frag.glsl";
+
+const drawTriangle = regl({
+  vert,
+  frag,
+  primitive: "triangles",
+  attributes: {
+    position: regl.buffer([
+      [0, 0],
+      [0, 1],
+      [1, 0],
+      [1, 1],
+    ]),
+  },
+  elements: regl.elements([0, 1, 2, 2, 1, 3]),
+});
+
+regl.frame(() => {
+  regl.clear({
+    color: [0, 0, 0, 1],
+  });
+  drawTriangle();
+});
