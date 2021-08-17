e27a1415557a4fc094bcf12c229f5e78a6c02d3b
Tomasz Wesołowski <kosashi@gmail.com>
2021-08-17 22:04:11 +0200

    Multiple draw calls

--- index.ts
+++ index.ts
@@ -4,25 +4,51 @@ const regl = initRegl();
 import vert from "./vert.glsl";
 // @ts-ignore
 import frag from "./frag.glsl";
+import REGL from "regl";
 
-const drawTriangle = regl({
+type Uniforms = {
+  Tick: number;
+  Aspect: number;
+  Translation: REGL.Vec2;
+};
+
+type Props = {
+  translation: REGL.Vec2;
+};
+
+const drawTriangle = regl<Uniforms, {}, Props>({
   vert,
   frag,
   primitive: "triangles",
   attributes: {
-    position: regl.buffer([
-      [0, 0],
-      [0, 1],
-      [1, 0],
+    Position: regl.buffer([
+      [-1, -1],
+      [-1, 1],
+      [1, -1],
       [1, 1],
     ]),
   },
   elements: regl.elements([0, 1, 2, 2, 1, 3]),
+  uniforms: {
+    Tick: ({ tick }) => tick,
+    Aspect: (context) => context.viewportWidth / context.viewportHeight,
+    Translation: (_context, props) => props.translation,
+  },
 });
 
 regl.frame(() => {
   regl.clear({
     color: [0, 0, 0, 1],
   });
-  drawTriangle();
+  drawTriangle([
+    {
+      translation: [0.5, 0],
+    },
+    {
+      translation: [0, 0],
+    },
+    {
+      translation: [-0.5, 0],
+    },
+  ]);
 });
--- vert.glsl
+++ vert.glsl
@@ -1,5 +1,21 @@
-attribute vec2 position;
+attribute vec2 Position;
+uniform float Tick;
+uniform float Aspect;
+uniform vec2 Translation;
+
+vec2 rotate(vec2 v, float a) {
+	float s = sin(a);
+	float c = cos(a);
+	mat2 m = mat2(c, -s, s, c);
+	return m * v;
+}
 
 void main() {
-    gl_Position = vec4(position, 0, 1);
+    vec2 pos = Position;
+    pos = rotate(pos, Tick*0.1);
+    pos *= 0.1;
+    pos += Translation;
+    pos *= vec2(1, Aspect);
+    
+    gl_Position = vec4(pos, 0, 1);
 }
